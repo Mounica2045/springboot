3489acc09bc13338ef67a912ac66f3d546e49517
Your Name <your@email.com>
2026-06-15 23:03:06 +0530

    Integrated HTML and CSS along with spring boot setup.

---
+++ src/main/java/com/mounica/Springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mounica.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
